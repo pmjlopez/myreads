bfb8a9e3624d95b986e8a70f0a5499e0f8bee1ce
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-08-24 15:06:36 +0800

    docs: removed notes

--- src/Search.js
+++ src/Search.js
@@ -47,14 +47,6 @@ class Search extends Component {
                 <form onSubmit={this.handleSubmit} className="search-books-bar">
                     <a className="close-search" href='/'>Close</a>
                     <div className="search-books-input-wrapper">
-                        {/*
-                          NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                          You can find these search terms here:
-                          https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                          However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                          you don't find a specific author or title. Every search is limited by search terms.
-                        */}
                         <input type="text" name='query' placeholder="Search by title or author"/>
                     </div>
                 </form>
