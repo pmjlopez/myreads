d0e9631c0c3b56195eb3a7f19c66721ced0dcdd1
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-08-24 15:05:30 +0800

    fix: let the function take as new shelf the value of none

--- src/Book.js
+++ src/Book.js
@@ -36,12 +36,12 @@ class Book extends Component {
                     <div className="book-shelf-changer">
                         <select
                             onChange={this.handleChange}
-                            value={book.shelf}>
+                            value={book.shelf ? book.shelf : 'none'}>
                             <option value="move" disabled>Move to...</option>
-                            <option value=''>None</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
+                            <option value='none'>None</option>
                         </select>
                     </div>
                 </div>
