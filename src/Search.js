a644d807518cdc8297513e4f084774d82ddd4798
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-08-24 20:49:07 +0800

    refactor: moved state up to App component

--- src/Search.js
+++ src/Search.js
@@ -1,61 +1,33 @@
 import React, { Component } from 'react';
-import * as BooksAPI from './BooksAPI';
 import Book from './Book';
-import serializeForm from 'form-serialize';
 
 class Search extends Component {
-    state = {
-        books: []
-    };
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const values = serializeForm(e.target, { hash: true });
-
-        if (values.query) {
-            console.log(`Searching for ${values.query}`);
-            BooksAPI
-            .search(values.query)
-            .then((books) => {
-                if (books.error) {
-                    console.log(`No books found.`);
-                    this.setState(() => ({
-                        books: []
-                    }));
-                } else {
-                    this.setState(() => ({
-                        books
-                    }));
-                }
-            });
-        } else {
-            alert('Please type the title or author.');
-        }
-    };
-    updateBook = (book) => {
-        BooksAPI.update(book, book.shelf);
-
-        let books = [...this.state.books];
-        let bookIndex = books.findIndex(b => b.id === book.id);
-        books[bookIndex] = book;
-        this.setState(() => ({
-            books
-        }));
-    };
     render() {
+        const {
+            books,
+            handleOnChange,
+            handleSubmit,
+            onChangeShelf
+        } = this.props;
         return(
             <div className="search-books">
-                <form onSubmit={this.handleSubmit} className="search-books-bar">
+                <form onSubmit={handleSubmit} className="search-books-bar">
                     <a className="close-search" href='/'>Close</a>
                     <div className="search-books-input-wrapper">
-                        <input type="text" name='query' placeholder="Search by title or author"/>
+                        <input
+                            type="text"
+                            name='query'
+                            placeholder="Search by title or author"
+                            onChange={handleOnChange}
+                        />
                     </div>
                 </form>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                        {this.state.books.map((book) => (
+                        {books.map((book) => (
                             <li key={book.id}>
                                 <Book
-                                    onUpdateBook={(book) => this.updateBook(book)}
+                                    onChangeShelf={onChangeShelf}
                                     book={book}/>
                             </li>
                         ))}
