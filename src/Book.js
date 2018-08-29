cb498b44681256e22dd137c73d9c80d338c76b09
Paul Michael John Lopez <pmjlopez@gmail.com>
2018-08-29 15:04:14 +0800

    refactor: use PureComponent instead of Component to prevent unnecessary re-renders

--- src/Book.js
+++ src/Book.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-const Book = (props) => {
-    const { onChangeShelf, book } = props;
-    return(
-        <div className="book">
-            <div className="book-top">
-                {book.imageLinks && (
-                    <div
-                        className="book-cover"
-                        style={{
-                            width: 128, height: 193,
-                            backgroundImage: `url(${book.imageLinks.thumbnail})`
-                        }}
-                    >
+class Book extends PureComponent {
+    componentDidMount () {
+        const { getBook, book } = this.props
+        getBook(book.id)
+    }
+    render () {
+        const { onChangeShelf, book } = this.props
+        return (
+            <div className="book">
+                <div className="book-top">
+                    {book.imageLinks && (
+                        <div
+                            className="book-cover"
+                            style={{
+                                width: 128, height: 193,
+                                backgroundImage: `url(${book.imageLinks.thumbnail})`
+                            }}
+                        >
+                        </div>
+                    )}
+                    <div className="book-shelf-changer">
+                        <select
+                            onChange={(event) => onChangeShelf(book, event.target.value)}
+                            value={book.shelf ? book.shelf : 'none'}>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value='none'>None</option>
+                        </select>
                     </div>
-                )}
-                <div className="book-shelf-changer">
-                    <select
-                        onChange={(event) => onChangeShelf(book, event.target.value)}
-                        value={book.shelf ? book.shelf : 'none'}>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value='none'>None</option>
-                    </select>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                    {book.authors && (
+                        book.authors.map((author) => (
+                            <span key={author}> {author} </span>
+                        ))
+                    )}
                 </div>
             </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">
-                {book.authors && (
-                    book.authors.map((author) => (
-                        <span key={author}> {author} </span>
-                    ))
-                )}
-            </div>
-        </div>
-    );
-};
+        )
+    }
+}
 
 export default Book;
